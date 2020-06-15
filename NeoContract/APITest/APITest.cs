03bfc2c1d26d7ae009762e639810e80051aff353
ProDog <gripzhang@outlook.com>
2020-06-15 18:35:17 +0800

    update

--- NeoContract/APITest/APITest.cs
+++ NeoContract/APITest/APITest.cs
@@ -11,24 +11,24 @@ namespace APITest
     [Features(ContractFeatures.HasStorage | ContractFeatures.Payable)]
     public partial class APITest : SmartContract
     {
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static string Name() => "SDK API TEST CONTRACT";
+        //[MethodImpl(MethodImplOptions.AggressiveInlining)]
+        //public static string Name() => "SDK API TEST CONTRACT";
 
-        private static byte[] Owner = "NikMd2j2bgVr8HzYgoJjbnwUPyXWnzjDCM".ToScriptHash();
+        //private static byte[] Owner = "NikMd2j2bgVr8HzYgoJjbnwUPyXWnzjDCM".ToScriptHash();
 
-        public static byte[] NeoToken = new byte[] { 0x89, 0x77, 0x20, 0xd8, 0xcd, 0x76, 0xf4, 0xf0, 0x0a, 0xbf, 0xa3, 0x7c, 0x0e, 0xdd, 0x88, 0x9c, 0x20, 0x8f, 0xde, 0x9b };
+        //public static byte[] NeoToken = new byte[] { 0x89, 0x77, 0x20, 0xd8, 0xcd, 0x76, 0xf4, 0xf0, 0x0a, 0xbf, 0xa3, 0x7c, 0x0e, 0xdd, 0x88, 0x9c, 0x20, 0x8f, 0xde, 0x9b };
 
-        public static byte[] GasToken = new byte[] { 0x3b, 0x7d, 0x37, 0x11, 0xc6, 0xf0, 0xcc, 0xf9, 0xb1, 0xdc, 0xa9, 0x03, 0xd1, 0xbf, 0xa1, 0xd8, 0x96, 0xf1, 0x23, 0x8c };
+        //public static byte[] GasToken = new byte[] { 0x3b, 0x7d, 0x37, 0x11, 0xc6, 0xf0, 0xcc, 0xf9, 0xb1, 0xdc, 0xa9, 0x03, 0xd1, 0xbf, 0xa1, 0xd8, 0x96, 0xf1, 0x23, 0x8c };
 
-        static byte[] bytes = "9bde8f209c88dd0e7ca3bf0af0f476cdd8207789".HexToBytes();
-        static byte[] bytes1 = Neo.SmartContract.Framework.Helper.HexToBytes("0x9bde8f209c88dd0e7ca3bf0af0f476cdd8207789");
-        static byte[] addressHash = Neo.SmartContract.Framework.Helper.ToScriptHash("NZ6A2ZLxKQY8hQxDFvuZkecBx8fj6MihS7");
-        static byte[] addressHash1 = Neo.SmartContract.Framework.Helper.ToScriptHash("NSzwm3ZZQNt7puaij6aq7hQ8EBD8r66XgF");
-        static byte[] addressHash2 = Neo.SmartContract.Framework.Helper.ToScriptHash("NLJNmdMBm5LR3J2gErmJzN3PF9qwAzFzCf");
+        //static byte[] bytes = "9bde8f209c88dd0e7ca3bf0af0f476cdd8207789".HexToBytes();
+        //static byte[] bytes1 = Neo.SmartContract.Framework.Helper.HexToBytes("0x9bde8f209c88dd0e7ca3bf0af0f476cdd8207789");
+        //static byte[] addressHash = Neo.SmartContract.Framework.Helper.ToScriptHash("NZ6A2ZLxKQY8hQxDFvuZkecBx8fj6MihS7");
+        //static byte[] addressHash1 = Neo.SmartContract.Framework.Helper.ToScriptHash("NSzwm3ZZQNt7puaij6aq7hQ8EBD8r66XgF");
+        //static byte[] addressHash2 = Neo.SmartContract.Framework.Helper.ToScriptHash("NLJNmdMBm5LR3J2gErmJzN3PF9qwAzFzCf");
 
         
 
-        public static object Test()
+        public static bool Test()
         {            
             return true;
         }
@@ -38,63 +38,63 @@ namespace APITest
         //    return c;
         //}
 
-        private static bool Test1(int a, int b)
-        {
-            return a < b;
-        }
+        //private static bool Test1(int a, int b)
+        //{
+        //    return a < b;
+        //}
 
-        private static (int, int) Test2(int a, int b)
-        {
-            return (a / b, a * b);
-        }
+        //private static (int, int) Test2(int a, int b)
+        //{
+        //    return (a / b, a * b);
+        //}
 
-        // 1 or 0
-        public static bool BoolTest()
-        {            
-            return true;
-        }
+        //// 1 or 0
+        //public static bool BoolTest()
+        //{            
+        //    return true;
+        //}
 
-        // 1 or 0
-        public static bool BoolTest1()
-        {            
-            return "aa" != "aa";
-        }
+        //// 1 or 0
+        //public static bool BoolTest1()
+        //{            
+        //    return "aa" != "aa";
+        //}
 
-        // true or false
-        public static bool BoolTest2()
-        {           
-            return new byte[] { 0x12 } != null;
-        }
+        //// true or false
+        //public static bool BoolTest2()
+        //{           
+        //    return new byte[] { 0x12 } != null;
+        //}
 
-        // true or false
-        public static bool BoolTest3()
-        {            
-            return new Object() == null;
-        }
+        //// true or false
+        //public static bool BoolTest3()
+        //{            
+        //    return new Object() == null;
+        //}
 
-        // true or false       
-        public static bool Exists(string message)
-        {
-            return Storage.Get(message) == null;
-        }
+        //// true or false       
+        //public static bool Exists(string message)
+        //{
+        //    return Storage.Get(message) == null;
+        //}
 
 
-        public static object TestAbort()
-        {
-            Runtime.Notify("test");
-            Abort();
-            Runtime.Notify(1);
-            return true;
-        }
+        //public static object TestAbort()
+        //{
+        //    Runtime.Notify("test");
+        //    Abort();
+        //    Runtime.Notify(1);
+        //    return true;
+        //}
 
-        public static object TestAssert(BigInteger bigInteger)
-        {
-            Runtime.Notify("test");
-            if (bigInteger == 1) Assert(true);
-            else Assert(false);
+        //public static object TestAssert(BigInteger bigInteger)
+        //{
+        //    Runtime.Notify("test");
+        //    if (bigInteger == 1) Assert(true);
+        //    else Assert(false);
 
-            Runtime.Notify(1);
-            return true;
-        }
+        //    Runtime.Notify(1);
+        //    return true;
+        //}
     }
 }
---
+++ NeoContract/ContractDemo/Class1.cs
@@ -0,0 +1,14 @@
+﻿using Neo.SmartContract.Framework;
+using System;
+
+namespace ContractDemo
+{
+    [Features(ContractFeatures.HasStorage | ContractFeatures.Payable)]
+    public class Class1 : SmartContract
+    {
+        public static bool Test()
+        {
+            return true;
+        }
+    }
+}
